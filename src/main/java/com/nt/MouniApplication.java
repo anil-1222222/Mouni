4295ad09b929f75c1b8459ecb501125c483ca3a0
Anil <anil@123>
2024-09-13 09:27:59 +0530

    Commit sb

---
+++ src/main/java/com/nt/MouniApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MouniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MouniApplication.class, args);
+	}
+
+}
